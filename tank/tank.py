e02c6d118babb111627929443fa94688339e8e9c
gracetangg <gracehtang@yahoo.com>
2023-11-08 15:45:52 -0500

    fix some class issues

--- tank/tank.py
+++ tank/tank.py
@@ -181,10 +181,10 @@ class Tank():
         # shouldn't need to define any of the messages anymore
         #  - should be handled by the interfaces functions
         # print("IPC DEFINE MSG: TEXTINPUT_START_MSG")
-        # IPC.IPC_defineMsg(TEXTINPUT_START_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_MESSAGE_FMT)
-        # IPC.IPC_defineMsg(TEXTINPUT_TEXT_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_MESSAGE_FMT)
-        # IPC.IPC_defineMsg(TEXTINPUT_KEYPRESS_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_MESSAGE_FMT)
-        # IPC.IPC_defineMsg(TEXTINPUT_CLEAR_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_MESSAGE_FMT)
+        IPC.IPC_defineMsg(TEXTINPUT_Start_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_Start_MSG_FMT)
+        IPC.IPC_defineMsg(TEXTINPUT_Text_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_Text_MSG_FMT)
+        IPC.IPC_defineMsg(TEXTINPUT_Keypress_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_Keypress_MSG_FMT)
+        IPC.IPC_defineMsg(TEXTINPUT_Clear_MSG, IPC.IPC_VARIABLE_LENGTH, TEXTINPUT_Clear_MSG_FMT)
         
     def publish_start_transcription(self):
         """
